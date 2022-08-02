fe98547f25359cfc3c0f9c283ed9dfc11577eb9c
Gabrielle Coelho Sanfilippo <gabriellecoelhosanfilippo@gmail.com>
2022-08-02 09:23:56 -0300

    test: test different values for steps

---
+++ src/components/Stepper/index.tsx
@@ -0,0 +1,38 @@
+import React, { useContext } from "react"
+
+import * as S from './styles'
+import { StepperContext, SteeperContextType } from "../../contexts/Stepper";
+
+interface IStep{
+    steps: number[],
+    labels: any[]
+}
+
+export default function Stepper({steps, labels}:IStep){
+    
+    const { numberSteps, setNumberSteps, stepLevel } = useContext<SteeperContextType>(StepperContext);
+
+    setNumberSteps(steps)
+
+    return(
+        <>
+            {numberSteps?.map((e: any)=>{
+                return(
+                    <S.Container>
+                        <S.Circle>
+                            {stepLevel > e+1 ? (
+                                <S.Finished><p>✔</p></S.Finished>
+                            ) : (
+                                <S.Opened></S.Opened>
+                            )}
+                        </S.Circle>
+                        <p>{labels[e]}</p>
+                    </S.Container>
+                    
+                )
+                
+            })}
+           
+        </>
+    )
+}
