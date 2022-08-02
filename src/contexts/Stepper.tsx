875a3591f3bf465bec55766e0f5ba2c851ca50f9
Gabrielle Coelho Sanfilippo <gabriellecoelhosanfilippo@gmail.com>
2022-08-02 09:23:18 -0300

    feat: emoji change according to step

--- src/contexts/Stepper.tsx
+++ src/contexts/Stepper.tsx
@@ -6,20 +6,25 @@ interface IChildren {
 } 
 
 export interface SteeperContextType {
-    stepLevel: number,
-    setStepLevel: Dispatch<SetStateAction<number>>
+    stepLevel: any,
+    setStepLevel: Dispatch<SetStateAction<any>>,
+    numberSteps: any,
+    setNumberSteps: Dispatch<SetStateAction<any>>
 }
 
 export const StepperContext = createContext<any>(null)
 
 export default function StepperProvider({children} : IChildren ) {
-    const [stepLevel, setStepLevel] = useState(4);
+    const [stepLevel, setStepLevel] = useState(1);
+    const [numberSteps, setNumberSteps] = useState()
 
     return(
         <StepperContext.Provider
             value = {{
                 stepLevel: stepLevel as Number,
-                setStepLevel: setStepLevel as Dispatch<SetStateAction<number>>
+                setStepLevel: setStepLevel as Dispatch<SetStateAction<number>>,
+                numberSteps: numberSteps as any,
+                setNumberSteps: setNumberSteps as Dispatch<SetStateAction<any>>
             }}
         >
             {children}
